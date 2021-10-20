3e3180a8d9908312df82f342d514e8ceee5d879f
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-10-20 19:53:45 -0300

    Finishing HighlightCard component

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,11 +1,20 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
 import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled.View`
-  background-color: ${props => props.theme.colors.shape};
+interface TypeProps {
+  type: 'up' | 'down' | 'total';
+}
+
+export const Container = styled.View<TypeProps>`
+  background-color: ${props => props.type === 'total' 
+    ? props.theme.colors.secondary 
+    : props.theme.colors.shape
+  };
+
   width: ${RFValue(300)}px;
   border-radius: 5px;
+
   padding: 19px 23px ${RFValue(42)}px;
   margin-right: 16px;
 `;
@@ -15,27 +24,52 @@ export const Header = styled.View`
   justify-content: space-between;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<TypeProps>`
   font-family: ${props => props.theme.fonts.regular};
   font-size: ${RFValue(14)}px;
   color: ${props => props.theme.colors.text_dark};
+
+  color: ${props => props.type === 'total' 
+    ? props.theme.colors.shape 
+    : props.theme.colors.text_dark
+  };
 `;
 
-export const Icon = styled(Feather)`
+export const Icon = styled(Feather)<TypeProps>`
   font-size: ${RFValue(40)}px;
+
+  ${props => props.type === 'up' && css`
+    color: ${props.theme.colors.success};
+  `}
+
+  ${props => props.type === 'down' && css`
+    color: ${props.theme.colors.attention};
+  `}
+
+  ${props => props.type === 'total' && css`
+    color: ${props.theme.colors.shape};
+  `}
 `;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TypeProps>`
   font-family: ${props => props.theme.fonts.medium};
   font-size: ${RFValue(32)}px;
-  color: ${props => props.theme.colors.text_dark};
   margin-top: 38px;
+
+  color: ${props => props.type === 'total' 
+    ? props.theme.colors.shape 
+    : props.theme.colors.text_dark
+  };
 `;
 
-export const LastTransaction = styled.Text`
+export const LastTransaction = styled.Text<TypeProps>`
   font-family: ${props => props.theme.fonts.regular};
   font-size: ${RFValue(12)}px;
-  color: ${props => props.theme.colors.text};
+
+  color: ${props => props.type === 'total' 
+    ? props.theme.colors.shape 
+    : props.theme.colors.text
+  };
 `;
