30e0d1e0482aeca63c55d8d48c1691153846f52a
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2021-10-20 19:25:27 -0300

    Positioning HighlightCard correctly

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
@@ -13,13 +14,16 @@ export const Header = styled.View`
   background: ${props => props.theme.colors.primary};
 
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
   width: 100%;
+
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -64,4 +68,9 @@ export const HighlightCards = styled.ScrollView.attrs({
   contentContainerStyle: { 
     paddingHorizontal: 24,
   },
-})``;
+})`
+  width: 100%;
+
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
